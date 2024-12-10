4b49c5a82aa4354c72cc91393c36615029ce1111
hggnomm <hggnomm.dev@gmail.com>
2024-12-10 16:02:10 +0700

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import WeatherApp from "./components/WeatherApp";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <WeatherApp />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./WeatherApp.css";
+import sunny from "../assets/sunny.png";
+import cloudy from "../assets/cloudy.png";
+import rainy from "../assets/rainy.png";
+import snowy from "../assets/snowy.png";
+
+const WeatherApp = () => {
+  return (
+    <div className="container">
+      <div className="weather-app">
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot"></i>
+            <div className="location">London</div>
+          </div>
+          <div className="search-bar">
+            <input type="text" placeholder="Enter Location" />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </div>
+          <div className="weather">
+            <img src={sunny} alt="sunny" />
+            <div className="weather-type">Clear</div>
+            <div className="temp">28°</div>
+          </div>
+          <div className="weather-date">
+            <p>Fri, 3 May</p>
+          </div>
+          <div className="weather-data">
+            <div className="humidity">
+              <div className="data-name">Humidity</div>
+              <i className="fa-solid fa-droplet"></i>
+              <div className="data">72%</div>
+            </div>
+            <div className="wind">
+              <div className="data-name">Wind</div>
+              <i className="fa-solid fa-wind"></i>
+              <div className="data">4 km/h</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherApp;
