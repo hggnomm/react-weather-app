faa24b5c4321609bb33bd8ae2d0f9e2e37336820
hggnomm <hggnomm.dev@gmail.com>
2024-12-10 16:57:54 +0700

    done css

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -18,25 +18,25 @@ const WeatherApp = () => {
             <input type="text" placeholder="Enter Location" />
             <i className="fa-solid fa-magnifying-glass"></i>
           </div>
-          <div className="weather">
-            <img src={sunny} alt="sunny" />
-            <div className="weather-type">Clear</div>
-            <div className="temp">28°</div>
+        </div>
+        <div className="weather">
+          <img src={sunny} alt="sunny" />
+          <div className="weather-type">Clear</div>
+          <div className="temp">28°</div>
+        </div>
+        <div className="weather-date">
+          <p>Fri, 3 May</p>
+        </div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humidity</div>
+            <i className="fa-solid fa-droplet"></i>
+            <div className="data">72%</div>
           </div>
-          <div className="weather-date">
-            <p>Fri, 3 May</p>
-          </div>
-          <div className="weather-data">
-            <div className="humidity">
-              <div className="data-name">Humidity</div>
-              <i className="fa-solid fa-droplet"></i>
-              <div className="data">72%</div>
-            </div>
-            <div className="wind">
-              <div className="data-name">Wind</div>
-              <i className="fa-solid fa-wind"></i>
-              <div className="data">4 km/h</div>
-            </div>
+          <div className="wind">
+            <div className="data-name">Wind</div>
+            <i className="fa-solid fa-wind"></i>
+            <div className="data">4 km/h</div>
           </div>
         </div>
       </div>
